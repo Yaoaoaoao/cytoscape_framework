5a059894dc19220c40904e80510b06bc0c716754
Jia Ren <renj@udel.edu>
2017-03-29 15:37:03 -0400

    Add support to dump set object

--- src/dumps.py
+++ src/dumps.py
@@ -3,12 +3,20 @@ import json
 _JS_OBJ = 'var eleObj = eval(%s);'
 
 
+def set_default(obj):
+    # json dump set()
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError
+
+
 def dump_network(NODES, EDGES):
     data = {
         "nodes": [{"data": n.__dict__} for n in NODES.values()],
         "edges": [{"data": e.__dict__} for e in EDGES.values()],
     }
-    return json.dumps(data)
+    return json.dumps(data, default=set_default)
+
 
 def web_dump_network(NODES, EDGES):
     return _JS_OBJ % dump_network(NODES, EDGES)
